3ab66d53fbe6c3b60e40d50ce1fbcad34fddb3b3
Naman Nigam <namannigam30@gmail.com>
2024-08-09 11:06:09 +0530

    app

--- backend/src/app.js
+++ backend/src/app.js
@@ -16,18 +16,30 @@ const allowedOrigins = [
 console.log('Allowed Origins:', allowedOrigins);
 
 // Dynamic CORS configuration
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error("Not allowed by CORS"));
+//       }
+//     },
+//     credentials: true,
+//   })
+// );
+
+app.use(cors({
+  origin: process.env.CORS_ORIGIN_PRODUCTION, // Replace with your specific origin
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
+  credentials: true, // Allow credentials if needed
+}));
+
+
+
+
+
 app.use((req, res, next) => {
   res.setHeader("Content-Type", "application/json");
   next();
