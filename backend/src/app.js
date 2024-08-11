a5374fabb70ff2118ca0f056d5663bdd7e855286
Naman Nigam <namannigam30@gmail.com>
2024-08-11 22:57:29 +0530

    CHANGED

--- backend/src/app.js
+++ backend/src/app.js
@@ -30,7 +30,7 @@ console.log('Allowed Origins:', allowedOrigins);
 // );
 
 app.use(cors({
-  origin: process.env.CORS_ORIGIN_LOCAL
+  origin: process.env.CORS_ORIGIN_PRODUCTION
   , methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed methods
   allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
   credentials: true, // Allow credentials if needed
