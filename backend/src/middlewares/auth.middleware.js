db8c651e76897426afe09a70df68f9d408e17f55
Naman Nigam <namannigam30@gmail.com>
2024-08-11 22:50:04 +0530

    updates

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -2,7 +2,8 @@ import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken";
 import { User } from "../models/user.model.js";
-
+import dotenv from "dotenv"
+dotenv.config();
 export const verifyJWT = asyncHandler(async (req, res, next) => {
   try {
     // Check if the request is from a guest user
