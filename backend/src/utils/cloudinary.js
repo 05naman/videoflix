fff8a7275059fd69d687e6a9be11c38c4f19d4ac
Naman Nigam <namannigam30@gmail.com>
2024-11-11 15:41:18 +0530

    cloudinary updated

--- backend/src/utils/cloudinary.js
+++ backend/src/utils/cloudinary.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv"
 dotenv.config();
 
 cloudinary.config({ 
+    secure:true,
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key:process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET
