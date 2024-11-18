74589d0261c44c62e62f6d56dee7317a59da5d95
Naman Nigam <namannigam30@gmail.com>
2024-11-19 01:29:17 +0530

    updated

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -89,8 +89,9 @@ function Header() {
           <span className="block h-[2px] w-full bg-white group-hover:bg-blue-400"></span>
         </button>
         <div
-          className={`fixed inset-y-0 right-0 flex w-full max-w-xs shrink-0 ${sideBar ? "translate-x-0" : "translate-x-full"
-            } flex-col border-l border-white bg-[#0e0e0e] duration-200 sm:static sm:ml-4 sm:w-auto sm:translate-x-0 sm:border-none`}
+          className={`fixed inset-y-0 right-0 flex w-full max-w-xs shrink-0 ${
+            sideBar ? "translate-x-0" : "translate-x-full"
+          } flex-col border-l border-white bg-[#0e0e0e] duration-200 sm:static sm:ml-4 sm:w-auto sm:translate-x-0 sm:border-none`}
         >
           <div className="relative flex w-full h-[4rem] items-center justify-end border-b border-white px-4 py-2 sm:hidden">
             <button
@@ -117,19 +118,23 @@ function Header() {
               ))}
             </ul>
           </IconContext.Provider>
-          <div className="mb-8 mt-auto flex w-full flex-wrap items-center justify-between gap-4 px-4 sm:mb-0 sm:mt-0 sm:px-0">
+          <div className="mb-8 mt-auto flex w-full flex-wrap items-center justify-end gap-4 px-4 sm:mb-0 sm:mt-0 sm:px-0">
+            {/* Upload Video Button */}
             <Button className="bg-green-800" onClick={handleUploadVideo}>
               Upload Video
             </Button>
 
             {authStatus && userData && (
               <div className="flex items-center gap-4">
+                {/* Logout Button */}
                 <Button
                   className="bg-red-700 hover:bg-red-500"
                   onClick={handleLogout}
                 >
                   Logout
                 </Button>
+
+                {/* Avatar Section */}
                 <Link
                   to={`/channel/${userData?.username}/videos`}
                   className="flex items-center gap-4 text-left"
@@ -149,18 +154,16 @@ function Header() {
 
             {!authStatus && (
               <Link to="/login">
-                <button className="bg-blue-800 mr-1 rounded px-3 py-2 text-center text-white transition-all duration-150 ease-in-out active:translate-x-[5px] active:translate-y-[5px] active:shadow-[0px_0px_0px_0px_#4f4e4e] sm:w-auto">
+                <button className="bg-blue-800 rounded px-3 py-2 text-center text-white transition-all duration-150 ease-in-out sm:w-auto">
                   Log in
                 </button>
               </Link>
             )}
           </div>
-
         </div>
       </nav>
     </header>
   );
-
 }
 
 export default Header;
