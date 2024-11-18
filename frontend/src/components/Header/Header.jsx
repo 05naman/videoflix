15e113e6830b39ef4a7202ff67b459538f2cfdd0
Naman Nigam <namannigam30@gmail.com>
2024-11-19 00:40:38 +0530

    update

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -118,8 +118,8 @@ function Header() {
               ))}
             </ul>
           </IconContext.Provider>
-          <div className="mb-8 mt-auto ml-20 flex w-full flex-wrap gap-4 px-4 sm:mb-0 sm:mt-0 sm:items-center sm:px-0">
-            <Button className="bg-green-800" onClick={handleUploadVideo}>Upload Video</Button>
+          <div className="mb-8 mt-auto ml-36 flex w-full flex-wrap gap-4 px-4 sm:mb-0 sm:mt-0 sm:items-center sm:px-0">
+            <Button className="bg-green-800 " onClick={handleUploadVideo}>Upload Video</Button>
   
             {authStatus && userData && (
               <>
